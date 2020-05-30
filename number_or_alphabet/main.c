2fbdf788f2029f8b22b9941b574ffbb3fb91238f
Rajiv <rajiv.ryadav@yahoo.com>
2020-05-30 15:21:49 +0530

    Code modified

--- number_or_alphabet/main.c
+++ number_or_alphabet/main.c
@@ -6,17 +6,17 @@ int main()
     char ca;
     printf("enter any character ");
     scanf("%c",&ca);
-    if (ca >= 'a' && ca <= 'z'||ca >= 'A' && ca <= 'Z')
+    if (ca >= 97 && ca <= 122||ca >= 65 && ca <= 97)
     {
     printf ("%c is a alphabet",ca);
     }
-    else if  (ca >= '0' && ca <= '9')
+    else if  (ca >= 48 && ca <= 57)
     {
       printf("%c is a number",ca);
     }
     else
     {
-        printf(" you have entered aspecial character ");
+        printf(" you have entered a special character ");
     }
 
     return 0;
